08889ed8f6ddd16c514d1735a615987785f0510d
Caio dias <caiodiasmartins12@gmail.com>
2023-12-28 15:43:18 -0300

    Lendo arquivo texto com classes File e Scanner

---
+++ Trabalhandocomarquivos/Programa.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Programa {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\caiod\\OneDrive\\Área de Trabalho\\Java\\Trabalhandocomarquivos\\good.txt");
+        Scanner sc = null;
+        try {
+            sc  = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Erro! = " + e.getMessage());
+        }
+        finally{
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    
+    }
+
+}
